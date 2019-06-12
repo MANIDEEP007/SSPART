f839b203ef31022584b97028785036cc10e261a0
Paduchuri Manideep <paduchurimanideep@gmail.com>
2019-06-12 22:04:16 +0530

    Update binary-search.java

--- binary-search.java
+++ binary-search.java
@@ -1,1 +1,33 @@
-
+import java.util.*;
+public class Main {
+  public static void search(String str_arr[],String ser_str)
+  {
+    int mid;
+    int low = 0, high = str_arr.length-1;
+    while(low<=high)
+    {
+      mid = (low+high)/2;
+      int status = ser_str.compareTo(str_arr[mid]);
+      if(status == 0)
+      {
+        System.out.println("String found at "+mid+" index");
+        return;
+      }
+      if(status > 0)
+      {
+        low = mid +1;
+      }
+      else
+      {
+        high = mid -1;
+      }
+    }
+    System.out.println("Not Found");
+  }
+  public static void main(String[] args) {
+    String str_arr[] = {"apple","banana","citrus","orange","peach","pear"};//Sample Array
+    Scanner s= new Scanner(System.in);
+    String ser_str = s.nextLine();
+    search(str_arr,ser_str);
+  }
+}
